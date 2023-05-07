a0945eee96e7069df3e43e1d23e5ec0d11ca82a9
MenSy6621 <130986974+MenSy6621@users.noreply.github.com>
2023-05-07 22:58:07 +0600

    Add files via upload

---
+++ js/Slider.js
@@ -0,0 +1,51 @@
+var swiper = new Swiper('.swiper', {
+  // // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoHeight: true,
+
+  // // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+  slidesPerGroup: 1,
+
+  slidesPerView: 1,
+});
+
+// var MySwiper = new Swiper('.swiper1', {
+//   loop: true,
+//   // // Optional parameters
+//   // direction: 'horizontal',
+//   // loop: true,
+
+//   // // If we need pagination
+//   // pagination: {
+//   //   el: '.swiper-pagination',
+//   // },
+
+//   // // Navigation arrows
+//   // navigation: {
+//   //   nextEl: '.swiper-button-next',
+//   //   prevEl: '.swiper-button-prev',
+//   // },
+
+//   // // And if we need scrollbar
+//   // scrollbar: {
+//   //   el: '.swiper-scrollbar',
+//   // },
+//   // slidesPerGroup: 1,
+
+//   // slidesPerView: 1,
+// });
